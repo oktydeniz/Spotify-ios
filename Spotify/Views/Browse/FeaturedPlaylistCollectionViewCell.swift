df4726678b1e2e6798b17f8a6e52d416d4b00715
oktydeniz <oktydeniz07@gmail.com>
2023-06-24 15:50:39 +0300

    adding sections in Home page

---
+++ Spotify/Views/Browse/FeaturedPlaylistCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlaylistCollectionViewCell.swift
+//  Spotify
+//
+//  Created by oktay on 24.06.2023.
+//
+
+import UIKit
+
+class FeaturedPlaylistCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "FeaturedPlaylistCollectionViewCell"
+    
+}
---
+++ Spotify/Views/Browse/NewReleasesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewReleasesCollectionViewCell.swift
+//  Spotify
+//
+//  Created by oktay on 24.06.2023.
+//
+
+import UIKit
+
+class NewReleasesCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "NewReleasesCollectionViewCell"
+    
+}
